e8ad6f96e23dbd2467333b93a1f313deef0fc56b
pafz <paferza@gmail.com>
2023-09-17 21:56:13 +0200

    simulation process finished

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+# React + Vite redux
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
@@ -7,7 +7,7 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-- PRUEBA TÉCNICA
+- SIMULACIÓN PRUEBA TÉCNICA:
 
 Desde el departamento de IT de The Bridge nos hemos lanzado al sector e-commerce y queremos tener listo cuanto antes un MVP.
 
@@ -21,8 +21,18 @@ Price
 Proporcionando ordenación en todas las columnas y paginación.
 
 Y que listemos los productos como en esta APP que ha visto:
+![foto](./src/assets/tbl.png)
 
+Para esta primera versión no se va a valorar la calidad del diseño, puesto que nuestro diseñador se encuentra de vacaciones.
+(Simulación realizada al final del bootcamp the full stack en The Bridge, encargado por Sofía Pinilla.)
+<views id="contact">
+<strong>Contacta</strong>
 
+  <p align="justify">
+Patricia Fernández
 
-Para esta primera versión no se va a valorar la calidad del diseño, puesto que nuestro diseñador se encuentra de vacaciones. 
+<a href="https://www.linkedin.com/in/patricia-fernandez-zamanillo/" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"></a>
 
+</p>
+<p align="right">(<a href="README.md#top">volver al principio </a>)</p>
+</views>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,16 +1,124 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getProducts } from '../../features/products/productsSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import './../../App.css';
 
 const Products = () => {
   const dispatch = useDispatch();
-  const products = useSelector(state => state.products);
+  const { products } = useSelector(state => state.products);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 5;
+  const [orderProp, setOrderProp] = useState('title');
 
   useEffect(() => {
     dispatch(getProducts());
   }, []);
-  console.log(products);
-  return <div>asd</div>;
+
+  const printItem = item => {
+    return (
+      <tr key={item.id}>
+        <td>{item.title}</td>
+        <td>{item.price}€</td>
+        <td>{item.category}</td>
+      </tr>
+    );
+  };
+
+  const printButton = pageNum => {
+    return (
+      <li key={pageNum}>
+        <button
+          onClick={() => {
+            setCurrentPage(pageNum - 1);
+          }}
+        >
+          {pageNum}
+        </button>
+      </li>
+    );
+  };
+
+  const buttons = [];
+  const pages = products.length / itemsPerPage;
+  for (let i = 1; i <= pages; i++) {
+    buttons.push(printButton(i));
+  }
+
+  const productsToPrint = products
+    .toSorted((a, b) => {
+      if (a[orderProp] < b[orderProp]) {
+        return -1;
+      }
+      if (a[orderProp] > b[orderProp]) {
+        return 1;
+      }
+      return 0;
+    })
+    .slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
+
+  const rows = productsToPrint.map(printItem);
+
+  const table = (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th
+              className={orderProp === 'title' ? 'sorting' : ''}
+              onClick={() => {
+                setOrderProp('title');
+              }}
+            >
+              Title
+            </th>
+            <th
+              className={orderProp === 'price' ? 'sorting' : ''}
+              onClick={() => {
+                setOrderProp('price');
+              }}
+            >
+              Price
+            </th>
+            <th
+              className={orderProp === 'category' ? 'sorting' : ''}
+              onClick={() => {
+                setOrderProp('category');
+              }}
+            >
+              Caregory
+            </th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+      <ul className="pages">
+        <li>
+          <button
+            onClick={() => {
+              if (currentPage > 0) {
+                setCurrentPage(currentPage - 1);
+              }
+            }}
+          >
+            &lt;
+          </button>
+        </li>
+        {buttons}
+        <li>
+          <button
+            onClick={() => {
+              if (currentPage < pages - 1) {
+                setCurrentPage(currentPage + 1);
+              }
+            }}
+          >
+            &gt;
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+  return <div>{table}</div>;
 };
 
 export default Products;
